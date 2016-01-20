d41a00819fb8f18173e8633572a87b65ee672f87
sorabh <sorabhs2009@gmail.com>
2016-01-20 12:41:33 +0530

    added

--- src/main/java/com/teamtrack/location/rest/LocationController.java
+++ src/main/java/com/teamtrack/location/rest/LocationController.java
@@ -1,5 +1,17 @@
 package com.teamtrack.location.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/location")
 public class LocationController {
 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String welcome(){
+		return "Lcaotion service is up and running";
+	}
+	
 }
