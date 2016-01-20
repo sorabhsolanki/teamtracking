e322e7a39b9114eaac76af37f4ba259fa8c64994
sorabh <sorabhs2009@gmail.com>
2016-01-20 14:08:42 +0530

    added

--- src/main/java/com/teamtrack/dto/Location.java
+++ src/main/java/com/teamtrack/dto/Location.java
@@ -5,28 +5,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Location {
 	
-	String title;
-	String singer;
+	private Locations locations;
 
-	public String getTitle() {
-		return title;
+	public Locations getLocations() {
+		return locations;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getSinger() {
-		return singer;
-	}
-
-	public void setSinger(String singer) {
-		this.singer = singer;
-	}
-
-	@Override
-	public String toString() {
-		return "Track [title=" + title + ", singer=" + singer + "]";
+	public void setLocations(Locations locations) {
+		this.locations = locations;
 	}
 
 }
---
+++ src/main/java/com/teamtrack/dto/Locations.java
@@ -0,0 +1,24 @@
+package com.teamtrack.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Locations {
+
+	private String userName;
+	private String distance;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	
+}
